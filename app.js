38eced6915ea7ae226057c5c1f9dab3f866e0bac
pbraswell <pbraswell@gmail.com>
2016-06-11 19:23:16 -0400

    Added a click handler to Channel

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 class Channel extends React.Component {
+  onClick(){
+    console.log('Hey!!!  I was clickTed: ', this.props.name);
+  }
   render() {
     return(
-      <li>{this.props.name}</li>
+      <li onClick={this.onClick.bind(this)}>{this.props.name}</li>
     )
   }
 }
 
-ReactDOM.render(<Channel name='Hardware Support'/>,
+ReactDOM.render(<Channel name='Hardware Supported'/>,
   document.getElementById('app'));
