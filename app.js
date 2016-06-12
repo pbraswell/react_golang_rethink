5e5ad289e6baadfec9ad3756a8eec152797c7ea6
pbraswell <pbraswell@gmail.com>
2016-06-12 11:12:32 -0400

    Added form to accept new channels.

--- app.js
+++ app.js
@@ -30,10 +30,30 @@ class ChannelList extends React.Component {
 }
 
 class ChannelForm extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {};
+  }
+  onChange(e){
+    this.setState({
+      channelName: e.target.value
+    });
+  }
+  onSubmit(e){
+    let {channelName} = this.state;
+    console.log(channelName);
+    // Clear the form
+    this.setState({
+      channelName: ''
+    });
+    e.preventDefault();
+  }
   render(){
     return(
-      <form>
-        <input type='text'/>
+      <form onSubmit={this.onSubmit.bind(this)}>
+        <input type='text'
+          onChange={this.onChange.bind(this)}
+          value={this.state.channelName}/>
       </form>
     )
   }
